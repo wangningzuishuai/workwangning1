12f6c8ee25cc15e3321f1d370eea6b845410acf3
wangning <wnzblo@163.com>
2021-04-07 16:21:34 +0800

    初始化

---
+++ springboot-dome1/src/main/java/com/leyou/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.leyou.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello spring boot";
+    }
+
+
+}
